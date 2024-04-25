81a8382d907708ac3400b882e03f4a474c195e8f
Your Name <you@example.com>
2024-04-25 13:47:21 +0300

    first commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let name = prompt ("Ismingizni kiriting");
-let name1 = prompt ("Bironta harf kiriting");
-let test = name1.includes(name)
+let name = prompt("Ismingizni kiriting").toLocaleLowerCase();
+let name1 = prompt("Bironta harf kiriting").toLocaleLowerCase();
+let test = name.includes(name1)
 
 if (test) {
-    alert("Sizning ismingizda bu harif mavjud...");
-}else {
-    alert("Sizning ismingizda bu harif mavjud emas...");
+    alert("Sizning ismingizda " + name1 + " bu harif mavjud...");
+} else {
+    alert("Sizning ismingizda " + name1 + " bu harif mavjud emas ...");
 }
